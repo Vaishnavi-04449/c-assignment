3b5f43bc6ed9893eed9b5de7015fa49997c1156b
Vaishnavi-04449 <vaishnavias204@gmail.com>
2026-03-16 16:31:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+class Student
+{
+    public string Name;
+    public int Age;
+    public int Marks;
+
+    public void GetGrade()
+    {
+        string grade = Marks switch
+        {
+            >= 90 => "A",
+            >= 75 => "B",
+            >= 50 => "C",
+            _ => "Fail"
+        };
+
+        Console.WriteLine("Grade: " + grade);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number of students: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            Student s = new Student();
+
+            Console.WriteLine("\nEnter details for Student " + i);
+
+            Console.Write("Enter Student Name: ");
+            s.Name = Console.ReadLine();
+
+            Console.Write("Enter Age: ");
+            s.Age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Marks: ");
+            s.Marks = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nStudent Name: " + s.Name);
+            Console.WriteLine("Age: " + s.Age);
+            Console.WriteLine("Marks: " + s.Marks);
+
+            s.GetGrade();
+        }
+    }
+}
